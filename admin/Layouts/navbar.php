48a124132c662cd3c6aff5bab967c6296423d659
erlangga277 <eangga606@gmail.com>
2025-05-10 20:35:33 +0700

    connect to landing page

--- admin/Layouts/navbar.php
+++ admin/Layouts/navbar.php
@@ -21,7 +21,7 @@ $activeUrl = $_GET['url'] ?? ''; // Get the current URL parameter or default to 
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
         <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+            <a class="nav-link" data-widget="navbar-search" href=".." role="button">
                 <i class="fas fa-search"></i>
             </a>
             <div class="navbar-search-block">
